52308b36bc53022494e9f4ed78ca24ed0eb686a1
Yizhou Sun <yizhou.sun78@gmail.com>
2025-03-29 03:52:48 -0400

    Update DueDateCalculator.test.ts

--- test/DueDateCalculator.test.ts
+++ test/DueDateCalculator.test.ts
@@ -8,7 +8,7 @@ describe('DueDateCalculator', () => {
     });
         
     test('should calculate correct due date within same day working hours', () => {
-        const submitDate = new Date('2025-03-28 09:00:00');
+        const submitDate = new Date('2025-03-28T09:00:00');
         const turnaroundHours = 8;
         const dueDate = calculator.calculateDueDate(submitDate, turnaroundHours);
         expect(dueDate.getDate()).toBe(28);
@@ -16,7 +16,7 @@ describe('DueDateCalculator', () => {
     });
 
     test('should calculate correct due date across multiple working days within working hours', () => {
-        const submitDate = new Date('2025-03-25 14:12:00');
+        const submitDate = new Date('2025-03-25T14:12:00');
         const turnaroundHours = 16;
         const dueDate = calculator.calculateDueDate(submitDate, turnaroundHours);
         expect(dueDate.getDate()).toBe(27);
@@ -25,7 +25,7 @@ describe('DueDateCalculator', () => {
     });
 
     test('should calculate correct due date across multiple working days within working hours', () => {
-        const submitDate = new Date('2025-03-25 12:15:00');
+        const submitDate = new Date('2025-03-25T12:15:00');
         const turnaroundHours = 5;
         const dueDate = calculator.calculateDueDate(submitDate, turnaroundHours);
         expect(dueDate.getDate()).toBe(26);
@@ -34,7 +34,7 @@ describe('DueDateCalculator', () => {
     });
 
     test('should calculate correct due date when due date falls on a weekend', () => {
-        const submitDate = new Date('2025-03-28 15:00:00');
+        const submitDate = new Date('2025-03-28T15:00:00');
         const turnaroundHours = 16;
         const dueDate = calculator.calculateDueDate(submitDate, turnaroundHours);
         expect(dueDate.getMonth()).toBe(3); // April 1st: getMonth() is 0-indexed
@@ -43,7 +43,7 @@ describe('DueDateCalculator', () => {
     });
 
     test('should throw error when submit date is outside working days', () => {
-        const submitDate = new Date('2025-03-29 09:00:00');
+        const submitDate = new Date('2025-03-29T09:00:00');
         const turnaroundHours = 8;
         expect(() => {
             calculator.calculateDueDate(submitDate, turnaroundHours);
@@ -51,7 +51,7 @@ describe('DueDateCalculator', () => {
     });
 
     test('should throw error when submit date is outside working hours', () => {
-        const submitDate = new Date('2025-03-28 08:00:00');
+        const submitDate = new Date('2025-03-28T08:00:00');
         const turnaroundHours = 8;
         expect(() => {
             calculator.calculateDueDate(submitDate, turnaroundHours);
@@ -59,7 +59,7 @@ describe('DueDateCalculator', () => {
     });
 
     test('should throw error when turnaround hours is negative', () => {
-        const submitDate = new Date('2025-03-28 09:00:00');
+        const submitDate = new Date('2025-03-28T09:00:00');
         const turnaroundHours = -8;
         expect(() => {
             calculator.calculateDueDate(submitDate, turnaroundHours);
@@ -67,7 +67,7 @@ describe('DueDateCalculator', () => {
     });
 
     test('should throw error when turnaround hours is less than or equal to 0', () => {
-        const submitDate = new Date('2025-03-28 09:00:00');
+        const submitDate = new Date('2025-03-28T09:00:00');
         const turnaroundHours = 0;
         expect(() => {
             calculator.calculateDueDate(submitDate, turnaroundHours);
